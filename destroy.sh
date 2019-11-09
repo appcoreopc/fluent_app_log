387e41689eb2aef167999315497989de37fccd7f
jeremy woo <kepungmoin@hotmail.com>
2019-11-09 16:32:45 +1300

    extrafixes

--- destroy.sh
+++ destroy.sh
@@ -1,2 +1,5 @@
 kubectl delete ClusterRoleBinding fluentd
+kubectl delete clusterrole fluentd
 kubectl delete serviceaccount fluentd
+kubectl delete DaemonSet fluentd --namespace kube-system
+
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-kubectl create -f service_account.yml --validate= false
+kubectl create -f service_account.yml --validate=false
 kubectl create -f cluster_role.yml
 kubectl create -f role_binding.yml
 kubectl create -f fluent_logger.yml
